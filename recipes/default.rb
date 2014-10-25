f42b23083c8294458aeee54149c8eb263677616f
shunsuke wada <letusfly95@gmail.com>
2014-10-25 14:18:03 +0900

    wgetを追加

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+bash "download gitlab deb" do
+  CODE <<-EOS
+    wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.4.2-omnibus-1_amd64.deb
+  EOS
+end
